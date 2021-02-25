f286eaaafdb4556e348c3ed25b2800129b700925
alfian ari putra <alfianputra19@gmail.com>
2021-02-25 20:47:24 +0700

    updated config file and soa

--- lua/config-template.lua
+++ lua/config-template.lua
@@ -1,9 +1,12 @@
 local _M = {}
 _M.database = {
-    host = "localhost",
-    username = "root",
+    host = "127.0.0.1",
+    username = "",
     password = "",
-    database = "pdns_blacklist",
+    database = "pdns-balcklist",
 }
-
+_M.record = {
+    ip_address = '192.168.1.1',
+    txt_messege = "\"Domain ini diblokir\""
+}
 return _M
--- lua/filter_engine.lua
+++ lua/filter_engine.lua
@@ -9,11 +9,20 @@
 pdnslog("pdns-recursor Lua script starting!", pdns.loglevels.Warning)
 driver = require "luasql.mysql"
 env = assert( driver.mysql() )
-local config = require "config"
+-- database configuration
+-- database = {
+--     host = "127.0.0.1",
+--     username = "alfian",
+--     password = "24april1997",
+--     database = "tes_filter",
+-- }
+config = dofile("config.lua")
+-- ip addresse for redirection
 
 function preresolve ( dq )
+    local stat = false
     -- create connection to database
-    koneksi = assert(env:connect(_G.config.database.host, _G.config.database.username, _G.config.database.password, _G.config.database.host))
+    koneksi = assert(env:connect(config.database.database, config.database.username, config.database.password, config.database.host))
     -- get query name
     domain = dq.qname:toString()
     -- remove last dot from query name
@@ -27,7 +36,7 @@ function preresolve ( dq )
     -- print('1')
     -- print(sth:fetch())
     -- check if true
-    if sth:fetch() == '1'
+    if (sth:fetch() == '1' and (dq.qtype == pdns.A or dq.qtype == pdns.A))
     then
         -- print('2')
         -- check if it A record
@@ -35,16 +44,15 @@ function preresolve ( dq )
         then
             -- rewrite the query
             -- print('3')
-            pdnslog("Query ",domain," Blocked", pdns.loglevels.Info)
-            dq:addAnswer(pdns.A, "192.168.10.22")
+            -- pdnslog("Query ",domain," Blocked", pdns.loglevels.Info)
+            dq:addAnswer(pdns.A, config.record.ip_address, 3600)
             -- message for domain dig
-            dq:addAnswer(pdns.TXT, "\"Domain ini diblokir\"", 3601)
+            dq:addRecord(pdns.TXT, config.record.txt_messege, 3600)
+            dq:addRecord(pdns.SOA,"fake."..dq.qname:toString().." fake."..dq.qname:toString().." 1 3600 900 1209600 86400",2)
             -- return tru if script rewite the answer
-            return true
-        else
-            return false
+            stat = true
         end
-    else
-        return false
     end
+    koneksi:close()
+    return stat
 end
